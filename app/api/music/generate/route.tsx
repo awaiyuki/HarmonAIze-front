f329d0eaeb341ce6e64fd8419132975663a18d36
harutea <snowhanami@gmail.com>
2024-06-06 00:04:15 +0900

    implementing feature to generate/display music cover

--- app/api/music/generate/route.tsx
+++ app/api/music/generate/route.tsx
@@ -1,5 +1,15 @@
+import OpenAI from 'openai'
+
 export async function POST(req: Request) {
   const reqData = await req.formData()
+  const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
+  const coverImage = await openai.images.generate({
+    model: 'dall-e-3',
+    prompt: 'a music cover with a cat',
+    quality: 'standard',
+    n: 1,
+  })
+  console.log(coverImage.data)
   const res = await fetch(process.env.BACK_HOST + '/user/generate', {
     method: 'POST',
     headers: {},
