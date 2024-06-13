ca9b6679ba27a797ef35b278e3920303312f42a2
harutea <snowhanami@gmail.com>
2024-06-13 17:00:33 +0900

    updating UI

--- app/components/generate/audio_player_bottom.tsx
+++ app/components/generate/audio_player_bottom.tsx
@@ -1,11 +1,12 @@
 //@ts-nocheck
 'use client'
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import AudioPlayer from 'react-h5-audio-player'
 import 'react-h5-audio-player/lib/styles.css'
 import { useContext, useRef } from 'react'
 import { AudioContext } from '../../context/audio_context'
 import { useSession } from 'next-auth/react'
+import { FormatAlignJustify } from '@mui/icons-material'
 
 export default function AudioPlayerBottom() {
   const { audioData, setAudioData } = useContext(AudioContext)
@@ -25,21 +26,35 @@ export default function AudioPlayerBottom() {
         zIndex: 2,
       }}
     >
-      {/* audio 제목 */}
-      <AudioPlayer
-        // ref={audioPlayerRef}
-        autoPlay
-        src={audioData?.audioSrc}
-        onPlay={(e) => console.log('onPlay')}
-        style={{
+      <Box
+        sx={{
           width: '100%',
           backgroundColor: 'rgba(255, 255, 255, 0.2)',
           backdropFilter: 'blur(10px)',
           borderRadius: '32px',
           margin: '8px',
           boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
         }}
-      />
+      >
+        <Typography variant="h5" fontWeight="bold">
+          {audioData?.title} {audioData?.username}
+        </Typography>
+        <AudioPlayer
+          // ref={audioPlayerRef}
+          autoPlay
+          src={audioData?.audioSrc}
+          onPlay={(e) => console.log('onPlay')}
+          style={{
+            width: '100%',
+            backgroundColor: 'rgba(255, 255, 255, 0.0)',
+            border: '0px',
+            boxShadow: '0px',
+          }}
+        />
+      </Box>
     </Box>
   )
 }
